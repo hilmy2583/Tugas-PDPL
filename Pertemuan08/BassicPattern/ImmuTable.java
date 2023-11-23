d26ce482a97cbf86545baae73ad77f3375e392eb
hilmy2583 <muhammadhilmy2583@gmail.com>
2023-11-23 15:17:57 +0700

    Pertemuan08

---
+++ Pertemuan08/BassicPattern/ImmuTable.java
@@ -0,0 +1,41 @@
+package Pertemuan08.BassicPattern;
+
+import java.lang.reflect.Constructor;
+
+public class ImmuTable {
+    private final String firstName;
+    private final String lastName;
+    private final String SSN;
+    private final String address;
+    private final Car car;
+
+    // Constructor
+    public ImmuTable(String fn, String ln, String ssn, String addr, Car c){
+        firstName = fn;
+        lastName = ln;
+        SSN = ssn;
+        address = addr;
+        car = c;
+    }
+
+    // Getters
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getSSN(){
+        return SSN;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public Car getCar(){
+        return (Car) car.clone();
+    }
+}
